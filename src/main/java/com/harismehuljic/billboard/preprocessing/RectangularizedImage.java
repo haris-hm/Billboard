7fc57567d98e1344e727bac93a97f2056f11c154
haris-hm <haris.mehuljic@drake.edu>
2025-07-08 15:10:33 -0500

    Attempts at implementing a rectangle packing algorithm

--- src/main/java/com/harismehuljic/billboard/preprocessing/RectangularizedImage.java
+++ src/main/java/com/harismehuljic/billboard/preprocessing/RectangularizedImage.java
@@ -4,6 +4,8 @@ import com.harismehuljic.billboard.rendering.Canvas;
 import net.minecraft.entity.decoration.DisplayEntity;
 
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * Represents and image where connected pixels of the same color are efficiently grouped into rectangles.
@@ -22,10 +24,41 @@ public class RectangularizedImage extends Image {
 
     @Override
     public void processImage(BufferedImage image) {
+        HashSet<int[]> visitedPixels = new HashSet<>();
 
+        for (int y = 0; y < this.height; y++) {
+            for (int x = 0; x < this.width; x++) {
+
+            }
+        }
     }
 
-    private void rectangularizeRegion(BufferedImage image, int startX, int startY) {
+    private void floodFillRectangle(BufferedImage image, int x, int y, int color, HashSet<int[]> visitedPixels) {
+        for (int i = x; i < this.width; i++) {
 
+        }
     }
+
+//    private void floodFillRectangle(BufferedImage image, int x, int y, int color) {
+//        ArrayList<ArrayList<Boolean>> visitedPixels = new ArrayList<>();
+//        floodFillRectangleHelper(image, x, y, color, visitedPixels);
+//    }
+//
+//    private void floodFillRectangleHelper(BufferedImage image, int x, int y, int color, ArrayList<ArrayList<Boolean>> visitedPixels) {
+//        int[] pixelCoord = new int[]{x, y};
+//
+//        if (x < 0 || x >= this.width || y < 0 || y >= this.height || image.getRGB(x, y) != color) {
+//            visitedPixels.get(y).add(false);
+//            return;
+//        }
+//
+//        if (visitedPixels.size() <= y) {
+//            visitedPixels.add(new ArrayList<>());
+//        }
+//
+//        visitedPixels.get(y).add(true);
+//
+//        floodFillRectangleHelper(image, x + 1, y, color, visitedPixels);
+//        floodFillRectangleHelper(image, x, y - 1, color, visitedPixels);
+//    }
 }
